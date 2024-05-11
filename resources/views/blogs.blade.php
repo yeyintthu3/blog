7b4e401fd192535d1762bcdd742aa5c7617179d9
yeyintthu <yeyint.thu3@frontiir.net>
2024-05-11 08:25:35 +0630

    testing git

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/blogs">First Blog</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto sunt voluptates ab voluptas nihil accusantium atque fugit. Culpa expedita ipsa eum obcaecati, sit exercitationem sequi voluptatem iusto aperiam molestiae rem laborum cum maxime aut quisquam voluptatibus ad nemo deserunt necessitatibus sint tempora maiores ullam. Doloribus rem aut unde nisi possimus praesentium eos cumque, doloremque quasi magnam odit nostrum nemo corrupti pariatur blanditiis labore voluptatum provident eum dolor, dignissimos neque numquam. Ut aut vitae sint laudantium dolorem dolorum eveniet est, laborum corrupti debitis dolore nesciunt harum velit suscipit dicta expedita magnam labore fuga repudiandae ab adipisci nam voluptatibus. Nemo, dolore laboriosam.</p>
+    </article>
+</body>
+</html>
